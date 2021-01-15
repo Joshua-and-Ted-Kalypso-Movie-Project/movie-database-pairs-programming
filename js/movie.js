e44a0c02c6a123082ad44fc5f44578f5d7f246aa
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-01-15 12:25:48 -0600

    I can use both terminal and GUI...

--- js/movie.js
+++ js/movie.js
@@ -14,9 +14,13 @@ function onSuccess(data, status) {
         <h5 class="">${data[i].rating} / 5 stars</h5> 
         <p>Starring: ${data[i].actors}</p>
         <p>Directed by: ${data[i].director}</p>
-        </div></div>`}
+        </div>
+        <div class="row"><button class="edit btn btn-primary col-5 m-1" id="edit${i}">Edit</button><button class="delete btn btn-primary col-5 m-1" id="delete${i}"}">Delete</button></div>
+        </div>`}
         return html})
     }
+
+
 
 
     //console.log(`Hello, ${name.toUpperCase()}!`); // Hello, CODEUP!
@@ -41,3 +45,20 @@ $.get("https://ruddy-enchanting-grasshopper.glitch.me/movies")
 $(document).ready(function() {
 
 })
+
+
+
+function addMovie() {
+$.post("https://ruddy-enchanting-grasshopper.glitch.me/movies", {
+    title: document.getElementById("movie-title").value,
+    rating: document.getElementById("movie-rating").value,
+    }).done(function(data) {
+        console.log(data);
+})
+}
+
+$("#add-movie").click(function(e) {
+    e.preventDefault();
+    addMovie();
+});
+
